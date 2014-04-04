f80938bd71c19994bca06036ee029ecf38566514
Craig L. Zirbel <zirbel@bgsu.edu>
2014-04-04 15:08:50 +0200

    zStringSplit

--- FR3DSource/zStringSplit.m
+++ FR3DSource/zStringSplit.m
@@ -12,10 +12,17 @@ c = 0;
 
 if isempty(strfind(s,delimiter)),
 	parts{1} = s;
-else
+elseif delimiter ~= char(9),
 	while ~isempty(remain),
 		c = c + 1;
 		[a,remain] = strtok(remain,delimiter);
 		parts{c} = a;
 	end
+else
+	j = find(s == char(9));
+	parts{1} = s(1:(j(1)-1));
+	for k = 1:(length(j)-1),
+		parts{k+1} = s((j(k)+1):(j(k+1)-1));
+	end
+	parts{length(j)+1} = s((j(end)+1):end);
 end
