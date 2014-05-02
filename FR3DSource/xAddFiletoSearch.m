8894fc5537e230e166f4c7dcc8408c186be2b327
Craig L. Zirbel <zirbel@bgsu.edu>
2014-05-02 16:37:41 +0200

    More efficient saving of FR3D searches

--- FR3DSource/xAddFiletoSearch.m
+++ FR3DSource/xAddFiletoSearch.m
@@ -42,81 +42,90 @@ else
       Search.File(f).Covalent = sparse([],[],[],N,N);
       Search.File(f).Backbone = sparse([],[],[],N,N);
       Search.File(f).Coplanar = sparse([],[],[],N,N);
-
-    end
-  
-    for i = 1:s,
-      f = double(Candidates(i,N+1));             % file number for this candidate
-  
       Search.CandidateFilenames{f} = File(f).Filename;
       Search.File(f).Filename      = File(f).Filename;
       Search.File(f).PDBFilename   = File(f).PDBFilename;
       Search.File(f).NumNT         = File(f).NumNT; % max number, some empty
       Search.File(f).Info          = File(f).Info;
 
+      AllIndices{f} = [];
+      AllOrigIndices{f} = [];
+      AllAA{f} = [];
+    end
+  
+  tic
+
+    for i = 1:s,                           % loop through candidates
+      f = double(Candidates(i,N+1));       % file number for this candidate
+  
       Indices = Cand(i,1:N);               % indices of nucleotides
 
       Indices = [Indices xNeighborhood(File(f),Indices,[1 0 0 1 1 1 1 16])];  % largest neighborhood
 
-      for j = Indices,
-        Search.File(f).NT(j) = File(f).NT(j);
-        for k = Indices,
-          Search.File(f).Edge(j,k) = File(f).Edge(j,k);
-          Search.File(f).Edge(k,j) = File(f).Edge(k,j);
-          Search.File(f).BasePhosphate(j,k) = File(f).BasePhosphate(j,k);
-          Search.File(f).BasePhosphate(k,j) = File(f).BasePhosphate(k,j);
-          Search.File(f).BaseRibose(j,k) = File(f).BaseRibose(j,k);
-          Search.File(f).BaseRibose(k,j) = File(f).BaseRibose(k,j);
-          Search.File(f).Range(j,k) = File(f).Range(j,k);
-          Search.File(f).Range(k,j) = File(f).Range(k,j);
-          Search.File(f).Covalent(j,k) = File(f).Covalent(j,k);
-          Search.File(f).Covalent(k,j) = File(f).Covalent(k,j);
-          Search.File(f).Crossing(j,k) = File(f).Crossing(j,k);
-          Search.File(f).Crossing(k,j) = File(f).Crossing(k,j);
-          Search.File(f).Backbone(j,k) = File(f).Backbone(j,k);
-          Search.File(f).Backbone(k,j) = File(f).Backbone(k,j);
-          Search.File(f).Coplanar(j,k) = File(f).Coplanar(j,k);
-          Search.File(f).Coplanar(k,j) = File(f).Coplanar(k,j);
-        end
-      end
-
-      % include nearby amino acids, for xDisplayCandidates
-
-      if isfield(File(f),'AA'),                
-
-% File(f).Filename
-
-        c = cat(1,File(f).NT(Indices).Center);      % nucleotide centers
-        if length(File(f).AA) > 1,
-          a = cat(1,File(f).AA.Center);               % amino acid centers
-          D = zDistance(c,a);                         % NT-AA distances
-          [i,j,k] = find(D);                          % 
-          w = find(k < 8);                            % within 8 Angstroms
-          u = unique(j(w));                           % indices of amino acids
-          Search.File(f).AA(u) = File(f).AA(u);       % add these AAs
-        end
-      end
-
-      if ~isfield(Search.File(f),'AA');
-        Search.File(f).AA = [];
-      end
+      AllIndices{f} = [AllIndices{f} Indices]; % accumulate indices needed for file f
+      AllOrigIndices{f} = [AllOrigIndices{f} Cand(i,1:N)];
 
       % include intervening nucleotides, if only a few, for alignments
   
       for n = 1:(N-1),                               % loop through nucleotides
-       if (MaxDiff(n) < Inf) || (maxinsert(n) < 5),   % if only few insertions
-        if double(Indices(n+1)) - double(Indices(n)) > 1,   % increasing order
-          for i = (Indices(n)+1):(Indices(n+1)-1),
-            Search.File(f).NT(i) = File(f).NT(i);
-          end
-        elseif double(Indices(n+1)) - double(Indices(n)) < -1, % dec order
-          for i = (Indices(n)-1):-1:(Indices(n+1)+1),
-            Search.File(f).NT(i) = File(f).NT(i);
+        if (MaxDiff(n) < Inf) || (maxinsert(n) < 5),   % if only few insertions
+          if double(Indices(n+1)) - double(Indices(n)) > 1,   % increasing order
+            AllIndices{f} = [AllIndices{f} (Indices(n)+1):(Indices(n+1)-1)];
+          elseif double(Indices(n+1)) - double(Indices(n)) < -1, % dec order
+            AllIndices{f} = [AllIndices{f} (Indices(n)-1):-1:(Indices(n+1)+1)];
           end
         end
-       end
       end
     end
+
+fprintf('Finding nearby nucleotides, ');
+toc
+zFlushOutput
+tic
+
+    for f = 1:length(AllIndices),
+      j = unique(AllIndices{f});        % only do each index once
+      Search.File(f).NT(j) = File(f).NT(j);
+      Search.File(f).Edge(j,j) = File(f).Edge(j,j);
+      Search.File(f).Edge(j,j) = File(f).Edge(j,j);
+      Search.File(f).BasePhosphate(j,j) = File(f).BasePhosphate(j,j);
+      Search.File(f).BasePhosphate(j,j) = File(f).BasePhosphate(j,j);
+      Search.File(f).BaseRibose(j,j) = File(f).BaseRibose(j,j);
+      Search.File(f).BaseRibose(j,j) = File(f).BaseRibose(j,j);
+      Search.File(f).Range(j,j) = File(f).Range(j,j);
+      Search.File(f).Range(j,j) = File(f).Range(j,j);
+      Search.File(f).Covalent(j,j) = File(f).Covalent(j,j);
+      Search.File(f).Covalent(j,j) = File(f).Covalent(j,j);
+      Search.File(f).Crossing(j,j) = File(f).Crossing(j,j);
+      Search.File(f).Crossing(j,j) = File(f).Crossing(j,j);
+      Search.File(f).Backbone(j,j) = File(f).Backbone(j,j);
+      Search.File(f).Backbone(j,j) = File(f).Backbone(j,j);
+      Search.File(f).Coplanar(j,j) = File(f).Coplanar(j,j);
+      Search.File(f).Coplanar(j,j) = File(f).Coplanar(j,j);
+    end
+
+fprintf('Adding nucleotides and interaction information, ');
+toc
+zFlushOutput
+
+    for f = 1:length(AllOrigIndices),
+      AllOrigIndices{f} = unique(AllOrigIndices{f});
+      if isfield(File(f),'AA') && length(File(f).AA) > 0,
+        c = cat(1,File(f).NT(AllOrigIndices{f}).Center);      % nucleotide centers
+        a = cat(1,File(f).AA.Center);               % amino acid centers
+        D = zDistance(c,a);                         % NT-AA distances
+        [i,j,k] = find(D);                          % 
+        w = find(k < 16);                           % within 16 Angstroms
+        u = unique(j(w));                           % indices of amino acids
+        Search.File(f).AA(u) = File(f).AA(u);       % add these AAs
+      else
+        Search.File(f).AA = [];
+      end
+    end
+
+fprintf('Finding nearby amino acids, ');
+toc
+zFlushOutput
 
   elseif ~isfield(Search,'CandidateFilenames'),
 
