4d284a6520f55b52aa8b27892a48b8fadec22f5b
Craig L. Zirbel <zirbel@bgsu.edu>
2015-05-21 17:39:03 -0400

    Try a new way of ordering instances

--- FR3DSource/zFindExemplarsPlot.m
+++ FR3DSource/zFindExemplarsPlot.m
@@ -1,7 +1,7 @@
 
       figure(8)
       clf
-      subplot(1,2,1)
+      subplot(1,3,1)
       cla
       q = zClusterGraph(PD);
       colormap('default');
@@ -24,7 +24,30 @@
 
       drawnow
 
-      subplot(1,2,2)
+      subplot(1,3,2)
+      cla
+      q = zClusterGraphNewOrdering(PD);
+      colormap('default');
+      map = colormap;
+      map = map((end-8):-1:8,:);
+      colormap(map);
+      caxis([0 1]);
+      colorbar('location','eastoutside');
+      hold on
+      j = find(q == i(1));
+      plot(j+0.5,j+0.5,'w*');
+      m = find(q == k(1));
+      plot(m+0.5,m+0.5,'wo');
+
+      title(['Discrepancy between instances of ' Pairs{pc} ' ' zEdgeText(CLE(row),Decimal,pc)]);
+      xlabel('Centroids marked (Geo discrep: white star, IDI: white circle)');
+      ylabel('Blue on diagonal means non coplanar');
+      FN = ['Instance_Discrepancies_' Pairs{pc} '_' zEdgeText(CLE(row),Decimal,pc)];
+      saveas(gcf,['Exemplars' filesep FN '.png'],'png');
+
+      drawnow
+
+      subplot(1,3,3)
       cla
       q = zClusterGraph(ID);
       colormap('default');
